95f0248f0421860dd8a27d2fc1b812b217d4cf9a
trevor-duane@github.com <trevorkayiira@gmail.com>
2023-08-16 13:12:23 +0300

    Homepage, responsive classes for mobile

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import HomeScreen from './pages/HomePage/HomeScreen';
+import { ThemeProvider } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']} minBreakpoint="xxs">
+      <HomeScreen />
+    </ThemeProvider>
+
   );
 }
 
